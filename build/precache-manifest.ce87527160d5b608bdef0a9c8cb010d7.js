0ff4cc481f701e702984c739ba10ca7669941dcd
Adrian <adrgon@gmail.com>
2020-08-22 12:54:08 -0300

    Add build

---
+++ build/precache-manifest.ce87527160d5b608bdef0a9c8cb010d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e012529c000471af753f6a439dad5191",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b103f21c7d2cc9f707b2",
+    "url": "/static/css/main.74e9db4f.chunk.css"
+  },
+  {
+    "revision": "012d0ec78dc11359e127",
+    "url": "/static/js/2.a9971af8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a9971af8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b103f21c7d2cc9f707b2",
+    "url": "/static/js/main.0f325690.chunk.js"
+  },
+  {
+    "revision": "ede226ef527d8d05b28f",
+    "url": "/static/js/runtime-main.b3af9155.js"
+  }
+]);
